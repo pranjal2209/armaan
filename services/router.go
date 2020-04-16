e835610441aee2d1b0344381b73352b4a0e14d1f
unknown <pranjal2209@gmail.com>
2020-04-17 02:02:00 +0530

    added vendor

--- services/router.go
+++ services/router.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	//"github.com/gorilla/mux"
 )
 
@@ -11,9 +12,18 @@ func HandlerFunc() {
 	//router := mux.NewRouter().StrictSlash(true)
 	//router := NewRouter().StrictSlash(true)
 	http.HandleFunc("/", HomePage)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(getPort(), nil))
 }
 
 func HomePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome home!")
+}
+func getPort() string {
+	var port = os.Getenv("PORT")
+	// Set a default port if there is nothing in the environment
+	if port == "" {
+		port = "5000"
+		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
+	}
+	return ":" + port
 }
